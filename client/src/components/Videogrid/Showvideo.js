93f68f442b39b519bb84c9e0f84c9e7e32c867c3
KetanSharma91 <ketansharma.ks.911@gmail.com>
2025-05-23 16:05:19 +0530

    Update Showvideo.js
    changed the url

--- client/src/components/Videogrid/Showvideo.js
+++ client/src/components/Videogrid/Showvideo.js
@@ -9,7 +9,7 @@ const Showvideo = ({ vid }) => {
     return (
         <>
             <Link to={`/videopage/${vid._id}`}>
-                <video src={`http://localhost:5000/${vid.filepath}`} className='video_ShowVideo' />
+                <video src={`https://youtube-ry5z.onrender.com/${vid.filepath}`} className='video_ShowVideo' />
             </Link>
             <div className="video_description">
                 <div className="Chanel_logo_App">
